42ea53ecf3a7bd4976b3e62f49b0061124d590de
Gabe Sumner <gsumner@gsumner-ltm7.internal.salesforce.com>
2019-04-18 14:36:01 -0700

    Changed scratch org live time to 30 days

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 7 -s -w 60
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
