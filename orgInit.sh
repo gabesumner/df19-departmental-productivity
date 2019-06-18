6f4ae67e86b38c823a7d587f7f10e82a2d900b68
mshanemc <shane.mclaughlin@salesforce.com>
2019-06-18 16:46:43 -0500

    script the dataflow, perms

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
-sfdx force:apex:execute -f scripts/assignAnalyticsPermset.apex
+sfdx shane:user:permset:assign -n analytics -g Integration -l User
 
 sfdx force:apex:execute -f scripts/generatePredictionAccounts.apex
 sfdx force:apex:execute -f scripts/generatePredictionAccounts.apex
@@ -11,7 +11,11 @@ sfdx force:apex:execute -f scripts/generatePredictionAccounts.apex
 sfdx force:apex:execute -f scripts/generatePredictionAccounts.apex
 sfdx force:apex:execute -f scripts/generateAccounts.apex
 
+sfdx force:source:deploy -p dataflow
+sfdx shane:analytics:dataflow:start -n Electron
+
 sfdx shane:theme:activate -n Electron
 sfdx force:org:open
 sfdx shane:user:password:set -g User -l User -p sfdx1234
-sfdx shane:analytics:dataflow:start -n Default
+
+sfdx force:source:deploy -p visualization
