9e660043fc33548dc94023580254b981480a1418
Gabe Sumner <gabe.sumner@gmail.com>
2019-10-01 16:02:02 -0700

    Removed unneeded Einstein Analytics steps.

--- orgInit.sh
+++ orgInit.sh
@@ -2,13 +2,8 @@ sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 60
 
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
-sfdx shane:user:permset:assign -n analytics -g Integration -l User
-
-sfdx force:source:deploy -p dataflow
-sfdx shane:analytics:dataflow:start -n Electron
 
 sfdx shane:theme:activate -n Electron
 sfdx force:org:open
-sfdx shane:user:password:set -g User -l User -p sfdx1234
+sfdx shane:user:password:set -g User -l User -p salesforce1
 
-sfdx force:source:deploy -p visualization
