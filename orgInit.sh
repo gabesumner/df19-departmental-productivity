845bafd7eda05751374bcfe2023bf4680fec0625
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-14 19:30:23 -0600

    load data

--- orgInit.sh
+++ orgInit.sh
@@ -6,3 +6,4 @@ sfdx force:user:permset:assign -n electron
 sfdx shane:theme:activate -n Electron
 sfdx force:org:open
 sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx automig:load -d data
